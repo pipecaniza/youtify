4da7fd6a39c40b61bbd880be173428e1dd9794ad
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2019-04-10 14:47:00 -0500

    Added basic components and dependencies

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Router, Route, Switch } from 'react-router';
+import history from '../history';
+import Home from './Home';
+import Header from './Header';
 
 const App = () => {
-    return (
-        <div>App</div>
-    );
+	return (
+		<div>
+			<Router history={history}>
+				<Header />
+				<Switch>
+					<Route path='/' exact component={Home} />
+				</Switch>
+			</Router>
+		</div>
+	);
 };
 
 export default App;
