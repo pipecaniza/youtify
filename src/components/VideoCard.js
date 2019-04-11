9b03efe5507cb849b3635edddb8eb1b6c6fd466b
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2019-04-10 23:27:00 -0500

    Added button for play

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -6,12 +6,8 @@ const VideoCard = ({ video }) => {
     <div className="card h-100" >
       <img src={video.thumbnail} className="card-img-top" alt="..." />
       <div className="card-body d-flex flex-column">
-        <h5 className="card-title">{video.title}</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>       
-        
-          <Link to="" className="btn btn-primary align-self-end">Play!</Link>
-        
-        
+        <h5 className="card-title">{video.title}</h5>           
+        <Link to="" className="btn btn-primary mt-auto btn-block">Play!</Link>
       </div>
     </div>
   );
